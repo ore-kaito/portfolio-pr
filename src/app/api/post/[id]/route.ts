a786ca8e383661dd4b52b5666a1483376c2f0fde
ore-kaito <>
2025-07-22 19:45:01 +0900

    Fix: DELETE route with valid context type

--- src/app/api/post/[id]/route.ts
+++ src/app/api/post/[id]/route.ts
@@ -1,19 +1,25 @@
-import { NextResponse } from 'next/server';
 import { prisma } from "../../lib/prisma";
+import { NextRequest, NextResponse } from 'next/server';
+
+
+type Context = {
+  params: {
+    id: string;
+  };
+};
 
 export async function DELETE(
-  req: Request,
-  context: { params: { id: string } }
-): Promise<Response> {
-  const { id } = context.params;
-  const postId = Number(id);
+  req: NextRequest,
+  context: Context
+): Promise<NextResponse> {
+  const postId = Number(context.params.id);
 
   try {
     await prisma.post.delete({
       where: { id: postId },
     });
 
-    return NextResponse.json({ message: 'Deleted successfully' }, { status: 200 });
+    return NextResponse.json({ message: 'Deleted successfully' });
   } catch (error) {
     console.error('削除エラー:', error);
     return NextResponse.json({ message: '削除に失敗しました' }, { status: 500 });
