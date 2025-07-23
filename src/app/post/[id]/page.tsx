4c6ee379ecd3602149ec42da9f31ad01e211b240
ore-kaito <>
2025-07-23 20:44:11 +0900

    aaa

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -2,16 +2,26 @@
 import { prisma } from "../../../lib/prisma";
 import { notFound } from "next/navigation";
 
-export default async function PostDetailPage({ params }: { params: { id: string } }) {
-  const post = await prisma.post.findUnique({ where: { id: Number(params.id) } });
+// type PostProps = {
+//   title: string;
+//   id : string;
+// };
 
-  if (!post) return notFound();
+// export default async function PostDetailPage({ params }: { params: { id: string } }) {
+//   const post = await prisma.post.findUnique({ where: { id: Number(params.id) } });
 
-  return (
-    <div className="max-w-xl mx-auto p-4">
-      <h1 className="text-2xl font-bold">{post.title}</h1>
-      <img src={post.imagePath || "/images/default.jpg"} alt={post.title} className="mt-4" />
-      <p className="mt-2 text-sm text-gray-600">{post.description}</p>
-    </div>
-  );
+//   if (!post) return notFound();
+
+//   return (
+//     <div className="max-w-xl mx-auto p-4">
+//       <h1 className="text-2xl font-bold">{post.title}</h1>
+//       <img src={post.imagePath || "/images/default.jpg"} alt={post.title} className="mt-4" />
+//       <p className="mt-2 text-sm text-gray-600">{post.description}</p>
+//     </div>
+//   );
+// }
+export default async function Page(props: { params: { id: string } }) {
+  const { params } = props;
+
+  return <h1>{params.id}</h1>;
 }
